df562a68817d3fb2f25ffe8c86eb53eb3e94eeb1
ehab elwazery <ehab.elwazery19@gmail.com>
2021-06-20 00:40:52 +0200

    crud completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-#root 'pages#home'
-root "articles#index"
-get "/articles", to: "articles#index"
-get "/articles/:id", to: "articles#show"
-match ':controller(/:action(/:id))',:via=>:get
+root 'pages#home'
+#root "articles#index"
+get "/about", to: "pages#about"
+#get "/articles/:id", to: "articles#show"
+resources :articles
+#match ':controller(/:action(/:id))',:via=>:get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

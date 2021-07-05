6222f722fa2ef4ee6622851279b576bfb3b04b54
ehab waziry <ehab.elwazery19@gmail.com>
2021-07-06 01:52:49 +0200

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ root 'pages#home'
 get "/about", to: "pages#about"
 #get "/articles/:id", to: "articles#show"
 resources :articles
+get 'signup', to: "users#new"
+resources :users, except:[:new]
 #match ':controller(/:action(/:id))',:via=>:get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

57a39f21bfde2264201034a30419c5f2035b0ccd
ehab waziry <ehab.elwazery19@gmail.com>
2021-07-14 03:49:17 +0200

    reverted after trying 2 passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_214025) do
+ActiveRecord::Schema.define(version: 2021_07_03_022028) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2021_07_13_214025) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.string "pass"
   end
 
 end
